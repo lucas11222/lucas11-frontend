613ef24eb7670b830ff9ee60b605ce228e47c3a5
BuildTools <altamiranolucas2013@gmail.com>
2025-03-27 14:50:07 +0100

    1.0.1, Analitycs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,20 @@ import react from '@astrojs/react';
 
 import vercel from '@astrojs/vercel';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
+
   integrations: [react()],
   adapter: vercel()
 });
+
+// @ts-ignore
+function tailwind(arg0) {
+  throw new Error('Function not implemented.');
+}
--- src/components/ui/app-sidebar.tsx
+++ src/components/ui/app-sidebar.tsx
@@ -1,4 +1,5 @@
-import { ChevronUp, Home, User2 } from "lucide-react";
+import { ChevronUp, GithubIcon, Home, User2, FileQuestion } from "lucide-react";
+import "@/assets/Discord-Symbol-White.svg";
 import {
   Sidebar,
   SidebarContent,
@@ -26,64 +27,120 @@ const helloworld = [
   },
 ];
 
+const socials = [
+  {
+    title: "Github",
+    url: "https://github.com/lucas11222",
+    icon: GithubIcon,
+  },
+];
+const projects = [
+  {
+    title: "Empry for now!",
+    url: "/",
+    icon: FileQuestion,
+  },
+];
 export function AppSidebar() {
   return (
-    <Sidebar>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem className="flex flex-row justify-center">
-            <img src="/logo.svg" width={120} height={120} alt="Author avatar" />
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
+    <>
+      <Sidebar>
+        <SidebarHeader>
+          <SidebarMenu>
+            <SidebarMenuItem className="flex flex-row justify-center">
+              <img
+                src="/logo.svg"
+                width={120}
+                height={120}
+                alt="Author avatar"
+              />
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarHeader>
 
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Hello world!</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {helloworld.map((helloworld) => (
-                <SidebarMenuItem key={helloworld.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={helloworld.url}>
-                      <helloworld.icon />
-                      <span>{helloworld.title}</span>
-                    </a>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel>Hello world!</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {helloworld.map((helloworld) => (
+                  <SidebarMenuItem key={helloworld.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={helloworld.url}>
+                        <helloworld.icon />
+                        <span>{helloworld.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+          <SidebarGroup>
+            <SidebarGroupLabel>Socials</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {socials.map((socials) => (
+                  <SidebarMenuItem key={socials.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={socials.url}>
+                        <socials.icon />
+                        <span>{socials.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+          <SidebarGroup>
+            <SidebarGroupLabel>Projects</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {projects.map((projects) => (
+                  <SidebarMenuItem key={projects.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={projects.url}>
+                        <projects.icon />
+                        <span>{projects.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+
+        <SidebarFooter>
+          <p className="text-center text-gray-700"></p>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton>
+                    <User2 /> Username
+                    <ChevronUp className="ml-auto" />
                   </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter>
-        <p className="text-center text-gray-700"></p>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuButton>
-                  <User2 /> Username
-                  <ChevronUp className="ml-auto" />
-                </SidebarMenuButton>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                side="top"
-                className="w-[--radix-popper-anchor-width]"
-              >
-                <DropdownMenuItem>
-                  <a href="/settings">Account</a>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <a type="submit" href="/api/signout">
-                    Sign out
-                  </a>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarFooter>
-    </Sidebar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  side="top"
+                  className="w-[--radix-popper-anchor-width]"
+                >
+                  <DropdownMenuItem>
+                    <a href="/settings">Account</a>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <a type="submit" href="/api/signout">
+                      Sign out
+                    </a>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      </Sidebar>
+    </>
   );
 }
