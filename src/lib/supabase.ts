534608641cab67de9e2c5a5063174de0c86ffa6f
BuildTools <altamiranolucas2013@gmail.com>
2025-05-04 16:50:13 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
+  "https://eerhkevdqolhculinrij.supabase.co",
   import.meta.env.SUPABASE_ANON_KEY,
   {
     auth: {
